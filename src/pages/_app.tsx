0c03c8fd55a61ff9d3b444c7b030ee8cf08a0089
Yu-Lun <chou522128@gmail.com>
2024-08-17 23:20:56 +0800

    add icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 import '../style/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
