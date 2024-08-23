6b1cfb4af6e7e0e48a706130e497ef9f73bd6b66
Yu-Lun <chou522128@gmail.com>
2024-08-24 01:35:56 +0800

    add navbar and search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,37 @@
-import { Button } from 'react-bootstrap';
+import { Container, Navbar, InputGroup, Form, Button, Image } from "react-bootstrap";
+import { HouseDoorFill } from "react-bootstrap-icons";
+import "../style/navbar.css"
 
 export default function Page() {
   return (
     <>
-      <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
+      <section id="navbar-section">
+        <Navbar className="navbar">
+          <Container>
+            <div>
+              <a className="d-flex" href="#">
+                <HouseDoorFill className="home-icon me-1" />
+                首頁
+              </a>
+            </div>
+            <div>
+              <a className="me-3" href="#">購物車</a>
+              <a className="me-3" href="#">登入</a>
+            </div>
+          </Container>
+        </Navbar>
+      </section>
+      <section id="search-section" className="d-flex align-items-center">
+        <Container className="d-flex align-items-center">
+          <Image className="logo me-4" alt="logo" src="logo.png"/>
+          <InputGroup>
+            <Form.Control placeholder="請輸入"/>
+            <Button className="btn-logo-blue">
+              搜尋
+            </Button>
+          </InputGroup>
+        </Container>
+      </section>
     </>
   );
 }
