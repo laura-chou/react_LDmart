53bebfe8503f0f6928f3cd1e11f3241a48cbf60a
Yu-Lun <chou522128@gmail.com>
2024-09-29 22:25:10 +0800

    show product info

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { HeartFill, CartPlusFill } from "react-bootstrap-icons";
+import styles from './products.module.css';
 
 interface ProductsData {
   id: number;
@@ -21,13 +28,32 @@ interface ProductsSectionProps {
 const ProductsSection: React.FC<ProductsSectionProps> = ({ allData }) => {
   return (
     <section>
-      <ul>
-        {allData.map((product) => (
-          <li key={product.id}>
-            {product.name}
-          </li>
-        ))}
-      </ul>
+      <Container>
+        <Row>
+          {allData.map((product) => (
+            <Col xs="auto" md="4" lg="4" key={product.id}>
+              <Card className={styles.card}>
+                <div className={styles.objectFitContainer}>
+                  <Card.Img variant="top" src={`products/1-1-${product.id}.jpg`} />
+                </div>
+                <Card.Body>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">{product.brandName}</Card.Subtitle>
+                  <Card.Text className={styles.cardText}>{product.description}</Card.Text>
+                  <div className="d-flex justify-content-end">
+                    <Button variant="primary" className='me-2'>
+                      <HeartFill/>
+                    </Button>
+                    <Button variant="primary">
+                      <CartPlusFill/>
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </section>
   )
 }
